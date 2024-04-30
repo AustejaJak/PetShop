b295bf52e69b3962d56e8c7973181cca42d62d71
Austeja Jakimaviciute <austejajak02@gmail.com>
2024-04-30 19:23:24 +0300

    Changing git structure

--- src/pages/client/signIn/SignIn.js
+++
@@ -1,7 +0,0 @@
-import SignInFormComponent from "../../../components/signInFormComponent/signInFormComponent";
-
-const SignInPage = () => {
-    return <SignInFormComponent />;
-};
-
-export default SignInPage;
