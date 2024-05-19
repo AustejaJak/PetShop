837dc5339a8b5367953077782880840fb0409246
Austeja Jakimaviciute <austejajak02@gmail.com>
2024-05-19 21:11:10 +0300

    Fixing merge conflicts

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8b6cac4b20c0bff962e3581124c7a87e2df9dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906f831672bd605038bc2cd31edbde6fa7a8f141")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
